267d4eb1f64c470674956bd64d3af192bda7420e
John Wells <molekewl2017@gmail.com>
2019-09-19 13:36:52 -0700

    set up formik state and one user input

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        React
+        <Form />
       </header>
     </div>
   );
